c28bd8a4fd08526c99152565f58425fe1e8b3ce8
shashank1623 <86946068+shashank1623@users.noreply.github.com>
2022-04-17 21:21:30 +0530

    Update q3.cpp

--- week1/q3.cpp
+++ week1/q3.cpp
@@ -1,1 +1,49 @@
 //  https://onlinegdb.com/uh3eE2dGE
+
+
+
+#include <iostream>
+#include <vector> 
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    vector <int> arr(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int key;
+    cin>>key;
+   int count = 1;
+   if(arr[0]==key)
+   {
+       cout<<"present "<<count<<endl;
+   }
+   else
+   {
+       count++;
+       int p =2;
+       int j=2;
+       for(;j<n&&arr[j]<=key;j = pow(2,p++))
+       {
+           if(arr[j]==key)
+           {
+               cout<<"present "<<count<<endl;
+               break;
+           }
+           count++;
+           
+       }
+       if(j>n||arr[j]<key)
+       {
+           cout<<"Not present "<<count<<endl;
+       }
+   }
+    return 0;
+}
+
