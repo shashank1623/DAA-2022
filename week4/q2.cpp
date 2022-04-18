59462781a23a890bd4006c0a1e5d3f6f5434ee94
shashank1623 <86946068+shashank1623@users.noreply.github.com>
2022-04-18 10:05:21 +0530

    Create q2.cpp

---
+++ week4/q2.cpp
@@ -0,0 +1,57 @@
+//partially coded
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int partion(vector <int> arr, int p ,int r)
+{
+    int x = arr[r];
+    int i = p-1;
+    
+    for(int j = p; j<=r-1;j++)
+    {
+        if(arr[j]<=x)
+        {
+            i = i+1;
+            swap(arr[i],arr[j]);
+        }
+    }
+    swap(arr[i+1],arr[r]);
+    return i+1;
+}
+
+void quick(vector <int> arr, int p, int r)
+{
+    if(p<r)
+    {
+        int q = partion(arr,p,r);
+        quick(arr,p,q-1);
+        quick(arr,q+1,r);
+    }
+}
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        vector <int> arr(n);
+        for(int i = 0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        
+        quick(arr,0,n-1);
+        for(int i = 0;i<n;i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
